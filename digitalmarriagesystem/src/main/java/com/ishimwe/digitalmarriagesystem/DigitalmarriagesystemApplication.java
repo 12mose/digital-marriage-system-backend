a51b23caa11b83a34e32e5c74c50124cfd5c4c2a
12mose <mosesishimwe2002@gmail.com>
2026-02-27 23:44:44 +0200

    Initial backend setup for Digital Marriage Registration System

---
+++ digitalmarriagesystem/src/main/java/com/ishimwe/digitalmarriagesystem/DigitalmarriagesystemApplication.java
@@ -0,0 +1,13 @@
+package com.ishimwe.digitalmarriagesystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DigitalmarriagesystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DigitalmarriagesystemApplication.class, args);
+	}
+
+}
---
+++ digitalmarriagesystem/src/test/java/com/ishimwe/digitalmarriagesystem/DigitalmarriagesystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ishimwe.digitalmarriagesystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DigitalmarriagesystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
